e2291a780fb1311341b9d7de6cc7da92cbd6b9a2
josue <lotshangojosue@gmail.com>
2019-07-23 02:53:55 -0700

    Add files via upload

---
+++ js/master.js
@@ -0,0 +1,12 @@
+function menubar(){
+  var menu = document.querySelector(".menu_mobile_responsible");
+  var header = document.querySelector(".header");
+  header.style.height = '309px';
+  menu.style.display = 'block';
+}
+function closeMenu(){
+  var menu = document.querySelector(".menu_mobile_responsible");
+  var header = document.querySelector(".header");
+  header.style.height = '80px';
+  menu.style.display = 'none';
+}
